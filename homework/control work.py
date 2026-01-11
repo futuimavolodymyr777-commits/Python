9b5e6669b2689788b382e46194ed66df81f31a66
futuimavolodymyr777-commits <futuimavolodymyr777@gmail.com>
2026-01-11 13:45:54 +0100

    Update control work.py

--- homework/control work.py
+++ homework/control work.py
@@ -1,46 +1,82 @@
-# завдання 1 метри - сантиметри
-print("завдання 1: перетворення метрів у сантиметри")
-meters = float(input("введіть довжину в метрах: "))
-centimeters = meters * 100
-print("довжина в сантиметрах:", centimeters)
-input("натисніть Enter, щоб перейти до наступного завдання...\n")
+# Меню
+print("1 - Время")
+print("2 - Круг")
+print("3 - Приставки")
+print("4 - Файл")
+print("5 - Приветствие")
+print("6 - Температура")
 
-# завдання 2 фарба для чотирьох стін
-print("завдання 2: фарба для чотирьох стін")
-paint_one_wall = float(input("введіть кількість літрів фарби для однієї стіни: "))
-paint_four_walls = paint_one_wall * 4
-print("для фарбування чотирьох стін потрібно", paint_four_walls, "літрів фарби")
-input("натисніть Enter, щоб перейти до наступного завдання...\n")
+n = input("Выбор: ")
 
-# завдання 3 сума різниця добуток
-print("завдання 3: арифметичні операції")
-a = float(input("введіть перше число: "))
-b = float(input("введіть друге число: "))
-print("сума:", a + b)
-print("різниця:", a - b)
-print("добуток:", a * b)
-input("натисніть Enter, щоб перейти до наступного завдання...\n")
+# 1. До полуночи
+if n == "1":
+    s = int(input("Секунды: "))
+    left = 86400 - s
+    h = left // 3600
+    m = left % 3600 // 60
+    sec = left % 60
+    print(h, "ч", m, "м", sec, "с")
 
-# завдання 4 відсоток від числа
-print("завдання 4: Обчислення відсотка")
-value = float(input("введіть число: "))
-percent = float(input("введіть відсоток: "))
-result = value * percent / 100
-print("результат:", result)
-input("натисніть Enter, щоб перейти до наступного завдання...\n")
+# 2. Круг
+elif n == "2":
+    d = float(input("Диаметр: "))
+    r = d / 2
+    c = input("1-пл 2-пер: ")
+    if c == "1":
+        print(3.14 * r * r)
+    elif c == "2":
+        print(3.14 * d)
 
-# завдання 5 площа прямокутника
-print("завдання 5: площа прямокутника")
-width = float(input("введіть ширину прямокутника: "))
-height = float(input("введіть висоту прямокутника: "))
-area = width * height
-print("площа прямокутника:", area)
-input("натисніть Enter, щоб перейти до наступного завдання...\n")
+# 3. Приставки
+elif n == "3":
+    p = float(input("Цена: "))
+    k = int(input("Кол-во: "))
+    d = float(input("Скидка %: "))
+    np = p - p * d / 100
+    c = input("1-сумма 2-цена: ")
+    if c == "1":
+        print(np * k)
+    elif c == "2":
+        print(np)
 
-# завдання 6 периметр квадрата
-print("завдання 6: периметр квадрата")
-a = float(input("введіть довжину сторони квадрата: "))
-P = 4 * a
-print("периметр квадрата:", P)
+# 4. Файл
+elif n == "4":
+    gb = float(input("ГБ: "))
+    sp = float(input("бит/с: "))
+    t = gb * 1024 * 1024 * 1024 * 8 / sp
+    c = input("1-ч 2-м 3-с: ")
+    if c == "1":
+        print(t / 3600)
+    elif c == "2":
+        print(t / 60)
+    elif c == "3":
+        print(t)
 
-print("\n усі завдання виконані <3")
+# 5. Приветствие
+elif n == "5":
+    h = int(input("Часы: "))
+    if 0 <= h < 6:
+        print("Good Night")
+    elif 6 <= h < 13:
+        print("Good Morning")
+    elif 13 <= h < 17:
+        print("Good Day")
+    else:
+        print("Good Evening")
+
+# 6. Температура
+elif n == "6":
+    t = float(input("Темп: "))
+    if t < -10:
+        print("Дуже холодно")
+    elif t < 0:
+        print("Холодно")
+    elif t < 15:
+        print("Прохолодно")
+    elif t < 25:
+        print("Тепло")
+    else:
+        print("Спекотно")
+
+else:
+    print("Ошибка")
