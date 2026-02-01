b5ca4e72f4cdf35d6f728b3eb470fc0ad605fee5
futuimavolodymyr777-commits <futuimavolodymyr777@gmail.com>
2026-02-01 13:10:15 +0100

    Update prktika.py

--- class_work/prktika.py
+++ class_work/prktika.py
@@ -1,11 +1,11 @@
 import random
 
 #Завдання 1
-num = int(input("Завдання 1 введіть число: "))
+num = int(input("завдання 1 введіть число: "))
 for i in range(1, 11):
     print(num, "*", i, "=", num * i)
 
-print("\n--------------------\n")
+print("\n...............\n")
 
 #Завдання 2
 print("завдання 2 таблиця множення від 1 до 9")
@@ -21,7 +21,7 @@ n = int(input("завдання 3 скільки чисел ви хочете в
 max_num = None
 
 for i in range(n):
-    x = int(input("Введіть число: "))
+    x = int(input("введіть число: "))
     if max_num is None or x > max_num:
         max_num = x
 
@@ -30,12 +30,12 @@ print("Найбільше число:", max_num)
 print("\n............\n")
 
 #Завдання 4
-print("завдання 4 гра 'Вгадай число'")
+print("завдання 4 гра 'вгадай число'")
 secret = random.randint(1, 500)
 attempts = 0
 
 while True:
-    guess = int(input("введіть число (0 — вихід): "))
+    guess = int(input("введіть число (0 - вихід): "))
 
     if guess == 0:
         print("гру завершено")
@@ -44,32 +44,32 @@ while True:
     attempts += 1
 
     if guess < secret:
-        print("Загадане число більше")
+        print("загадане число більше")
     elif guess > secret:
-        print("Загадане число менше")
+        print("загадане число менше")
     else:
-        print("Ви вгадали число!")
-        print("Кількість спроб:", attempts)
+        print("ви вгадали число!")
+        print("кількість спроб:", attempts)
         break
 
 print("\n.............\n")
 
 #Завдання 5
 print("завдання 5 малювання фігури")
-figure = input("Введіть тип фігури (квадрат/прямокутник): ")
+figure = input("введіть тип фігури (квадрат/прямокутник): ")
 
-symbol = input("Введіть символ: ")
+symbol = input("введіть символ: ")
 
 if figure == "квадрат":
-    size = int(input("Введіть довжину сторони: "))
+    size = int(input("введіть довжину сторони: "))
     for i in range(size):
         print(symbol * size)
 
 elif figure == "прямокутник":
-    width = int(input("Введіть ширину: "))
-    height = int(input("Введіть висоту: "))
+    width = int(input("введіть ширину: "))
+    height = int(input("введіть висоту: "))
     for i in range(height):
         print(symbol * width)
 
 else:
-    print("Невірний тип фігури")
+    print("невірний тип фігури")
